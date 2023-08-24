f68866d65d146430228802604c20f257b42e8928
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-08-24 12:28:00 -0300

    Aula 4.4

--- analise_sentimentos.py
+++ analise_sentimentos.py
@@ -22,32 +22,37 @@ def analise_sentimento(nome_do_produto):
     print(f"Iniciando a análise do produto: {nome_do_produto}")
 
     tentativas = 0
+    tempo_de_espera = 5
     while tentativas < 3:
         tentativas += 1
         print(f"Tentativa {tentativas}")
         try:
-            raise openai.error.APIError
-            # resposta = openai.ChatCompletion.create(
-            #    model = "gpt-3.5-turbo",
-            #    messages = [
-            #        {
-            #            "role": "system",
-            #            "content": prompt_sistema
-            #        },
-            #        {
-            #            "role": "user",
-            #            "content": prompt_usuario
-            #        }
-            #   ]
-            # )
+            resposta = openai.ChatCompletion.create(
+               model = "gpt-3.5-turbo",
+               messages = [
+                   {
+                       "role": "system",
+                       "content": prompt_sistema
+                   },
+                   {
+                       "role": "user",
+                       "content": prompt_usuario
+                   }
+              ]
+            )
 
             salva(f"./dados/analise-{nome_do_produto}", resposta.choices[0].message.content)
             print("Análise concluída com sucesso!")
+            return
         except openai.error.AuthenticationError as e:
             print(f"Erro de autenticacao: {e}")
         except openai.error.APIError as e:
             print(f"Erro de API: {e}")
             time.sleep(5)
+        except openai.error.RateLimitError as e:
+            print(f"Erro de limite de taxa: {e}")
+            time.sleep(tempo_de_espera)
+            tempo_de_espera *= 2
 
 
 def carrega(nome_do_arquivo):
@@ -66,8 +71,8 @@ def salva(nome_do_arquivo, conteudo):
         print(f"Erro ao salvar arquivo: {e}")
 
 dotenv.load_dotenv()
-openai.api_key = ""
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-lista_de_produtos = ["Tapete de yoga", "Tabuleiro de xadrez de madeira"]
+lista_de_produtos = ["DVD player automotivo", "Esteira elétrica para fitness", "Grill elétrico para churrasco", "Mixer de sucos e vitaminas", "Tapete de yoga", "Miniatura de carro colecionável", "Balança de cozinha digital", "Jogo de copos e taças de cristal", "Tabuleiro de xadrez de madeira", "Boia inflável para piscina"]
 for nome_do_produto in lista_de_produtos:
     analise_sentimento(nome_do_produto)
