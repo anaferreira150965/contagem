ad03f09e9e080544054b63ad01e4c48956102bd4
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-08-24 11:04:57 -0300

    Aula 2.3

--- categorizador.py
+++ categorizador.py
@@ -2,41 +2,45 @@ import os
 import openai
 import dotenv
 
-dotenv.load_dotenv()
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-prompt_sistema = """
-Você é um categorizador de produtos.
-Você deve escolher uma categoria da lista abaixo:
-##### Lista de categorias válidas
-Beleza
-Entretenimento
-Esportes
-Outros
-##### Exemplo
-bola de tenis
-Esportes
-"""
-
-resposta = openai.ChatCompletion.create(
+def categorizaProduto(nome_do_produto, categorias_validas):
+    prompt_sistema = f"""
+    Você é um categorizador de produtos.
+    Você deve escolher uma categoria da lista abaixo:
+    Se as categorias informadas não forem categorias validas, responda com "Não posso ajudá-lo com isso"
+    ##### Lista de categorias válidas
+    {categorias_validas}
+    ##### Exemplo
+    bola de tênis
+    Esportes
+    """
+    resposta = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {
-          "role": "system",
-          "content": prompt_sistema
+        "role": "system",
+        "content": prompt_sistema
         },
         {
             "role": "user",
-            "content": "bola de tenis de mesa"
+            "content": nome_do_produto
         }
     ],
     temperature=1,
     max_tokens=256,
     top_p=1,
     frequency_penalty=0,
-    presence_penalty=0,
-    n=5
-)
-for i in range(0,5):
-    print(resposta.choices[i].message.content)
-    print("-------------------")
+    presence_penalty=0
+    )
+    print(resposta.choices[0].message.content)
+
+
+dotenv.load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+print("Digite as categorias validas:")
+categorias_validas = input()
+while True:
+    print("Digite o nome do produto:")
+    nome_do_produto = input()
+    categorizaProduto(nome_do_produto, categorias_validas)
