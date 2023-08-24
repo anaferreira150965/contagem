c397f9f47d7b2c858821778e8ccba669ba24f545
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-08-24 12:12:34 -0300

    Aula 3.3

--- identificador_de_perfil.py
+++ identificador_de_perfil.py
@@ -1,7 +1,9 @@
 import os
 import openai
 import dotenv
+import tiktoken
 
+codificador = tiktoken.encoding_for_model("gpt-3.5-turbo")
 def carrega(nome_do_arquivo):
     try:
         with open(nome_do_arquivo, "r") as arquivo:
@@ -21,10 +23,18 @@ O formato de saída deve ser:
 cliente - descreva o perfil do cliente em 3 palavras
 """
 
-prompt_usuario = carrega("./dados/lista_de_compras_10_clientes.csv")
+prompt_usuario = carrega("./dados/lista_de_compras_100_clientes.csv")
 
+lista_de_tokens = codificador.encode(prompt_sistema + prompt_usuario)
+numero_de_tokens = len(lista_de_tokens)
+print(f"Número de tokens na entrada: {numero_de_tokens}")
+modelo = "gpt-3.5-turbo"
+tamanho_esperado_saida = 2048
+if numero_de_tokens >= 4096 - tamanho_esperado_saida:
+    modelo = "gpt-3.5-turbo-16k"
+print(f"Modelo escolhido: {modelo}")
 resposta = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
+  model=modelo,
   messages=[
     {
       "role": "system",
@@ -36,7 +46,7 @@ resposta = openai.ChatCompletion.create(
     }
   ],
   temperature=1,
-  max_tokens=256,
+  max_tokens=tamanho_esperado_saida,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
