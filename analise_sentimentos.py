ec763e8ff00f9eb32c6ac68a7089319ff5bad8d6
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-08-24 12:27:14 -0300

    Aula 4.3

--- analise_sentimentos.py
+++ analise_sentimentos.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import dotenv
+import time
 
 def analise_sentimento(nome_do_produto):
     prompt_sistema = """
@@ -20,22 +21,34 @@ def analise_sentimento(nome_do_produto):
     prompt_usuario = carrega(f"./dados/avaliacoes-{nome_do_produto}.txt")
     print(f"Iniciando a análise do produto: {nome_do_produto}")
 
-    resposta = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {
-                "role": "system",
-                "content": prompt_sistema
-            },
-            {
-                "role": "user",
-                "content": prompt_usuario
-            }
-        ]
-    )
+    tentativas = 0
+    while tentativas < 3:
+        tentativas += 1
+        print(f"Tentativa {tentativas}")
+        try:
+            raise openai.error.APIError
+            # resposta = openai.ChatCompletion.create(
+            #    model = "gpt-3.5-turbo",
+            #    messages = [
+            #        {
+            #            "role": "system",
+            #            "content": prompt_sistema
+            #        },
+            #        {
+            #            "role": "user",
+            #            "content": prompt_usuario
+            #        }
+            #   ]
+            # )
 
-    salva(f"./dados/analise-{nome_do_produto}", resposta.choices[0].message.content)
-    print("Análise concluída com sucesso!")
+            salva(f"./dados/analise-{nome_do_produto}", resposta.choices[0].message.content)
+            print("Análise concluída com sucesso!")
+        except openai.error.AuthenticationError as e:
+            print(f"Erro de autenticacao: {e}")
+        except openai.error.APIError as e:
+            print(f"Erro de API: {e}")
+            time.sleep(5)
+
 
 def carrega(nome_do_arquivo):
     try:
@@ -53,7 +66,7 @@ def salva(nome_do_arquivo, conteudo):
         print(f"Erro ao salvar arquivo: {e}")
 
 dotenv.load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = ""
 
 lista_de_produtos = ["Tapete de yoga", "Tabuleiro de xadrez de madeira"]
 for nome_do_produto in lista_de_produtos:
