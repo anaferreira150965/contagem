069d4429fa2aa17226f494b9b8a9ae5119026eb6
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-08-24 14:45:29 -0300

    Aula 5.3

--- aula 5/recomendador.py
+++ aula 5/recomendador.py
@@ -30,6 +30,7 @@ def salva(nome_do_arquivo, conteudo):
         print(f"Erro ao salvar arquivo: {e}")
 
 def identifica_perfis(lista_de_compras_por_cliente):
+  print("1. Iniciando identificação de perfis")
   prompt_sistema = """
 Identifique o perfil de compra para cada cliente a seguir.
 
@@ -61,17 +62,47 @@ O formato de saída deve ser em JSON:
 
   conteudo = resposta.choices[0].message.content
   json_resultado = json.loads(conteudo)
+  print("Finalizou identificação de perfis")
   return json_resultado
+
+def recomenda_produtos(perfil, lista_de_produtos):
+  print("2. Iniciando recomendação de produtos")
+  prompt_sistema = f"""
+  Você é um recomendador de produtos.
+  Considere o seguinte perfil: {perfil}
+  Recomende 3 produtos a partir da lista de produtos válidos e que sejam adequados ao perfil informado.
+  
+  #### Lista de produtos válidos para recomendação
+  {lista_de_produtos}
+
+  A saída deve ser apenas o nome dos produtos recomendados em bullet points. 
+  """
+
+  resposta = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {
+        "role": "system",
+        "content": prompt_sistema
+      }
+    ]
+  )
+
+  conteudo = resposta.choices[0].message.content
+  print("2. Finalizando a recomendação de produtos")
+  return conteudo
     
 
 dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+lista_de_produtos = carrega("./dados/lista_de_produtos.txt")
 lista_de_compras_por_cliente = carrega("./dados/lista_de_compras_10_clientes.csv")
 perfis = identifica_perfis(lista_de_compras_por_cliente)
 
-for perfil in perfis["clientes"]:
-    nome_do_cliente = perfil["nome"]
+for cliente in perfis["clientes"]:
+    nome_do_cliente = cliente["nome"]
     print(f"Iniciando recomendação para o cliente {nome_do_cliente}")
-    #recomendacoes = recomenda_produtos(perfil, lista_de_produtos)
+    recomendacoes = recomenda_produtos(cliente["perfil"], lista_de_produtos)
+    print(recomendacoes)
     #email = escreve_email(recomendacoes)
