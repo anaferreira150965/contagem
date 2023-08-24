69860010d146d7a9268d97201edc7c3e725f3ff0
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-08-24 12:10:43 -0300

    Aula 3.3 - Base do Identificador de Perfil

---
+++ identificador_de_perfil.py
@@ -0,0 +1,45 @@
+import os
+import openai
+import dotenv
+
+def carrega(nome_do_arquivo):
+    try:
+        with open(nome_do_arquivo, "r") as arquivo:
+            dados = arquivo.read()
+            return dados
+    except IOError as e:
+        print(f"Erro: {e}")
+
+dotenv.load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+prompt_sistema = """
+Identifique o perfil de compra para cada cliente a seguir.
+
+O formato de saída deve ser:
+
+cliente - descreva o perfil do cliente em 3 palavras
+"""
+
+prompt_usuario = carrega("./dados/lista_de_compras_10_clientes.csv")
+
+resposta = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+      "role": "system",
+      "content": prompt_sistema
+    },
+    {
+      "role": "user",
+      "content": prompt_usuario
+    }
+  ],
+  temperature=1,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+print(resposta.choices[0].message.content)
