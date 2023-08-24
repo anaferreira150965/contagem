4c0610083ba6577aa8035672647221439016f638
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-08-24 12:26:21 -0300

    Aula 4.2

--- analise_sentimentos.py
+++ analise_sentimentos.py
@@ -2,6 +2,41 @@ import os
 import openai
 import dotenv
 
+def analise_sentimento(nome_do_produto):
+    prompt_sistema = """
+    Você é um analisador de sentimentos de avaliações de produtos.
+    Escreva um parágrafo com até 50 palavras resumindo as avaliações e depois atribua qual o sentimento geral para o produto.
+    Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+
+    #### Formato de saída
+
+    Nome do produto: 
+    Resumo das avaliações:
+    Sentimento geral: [aqui deve ser POSITIVO, NEUTRO ou NEGATIVO]
+    Pontos fortes: [3 bullet points]
+    Pontos fracos: [3 bullet points]
+    """
+
+    prompt_usuario = carrega(f"./dados/avaliacoes-{nome_do_produto}.txt")
+    print(f"Iniciando a análise do produto: {nome_do_produto}")
+
+    resposta = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {
+                "role": "system",
+                "content": prompt_sistema
+            },
+            {
+                "role": "user",
+                "content": prompt_usuario
+            }
+        ]
+    )
+
+    salva(f"./dados/analise-{nome_do_produto}", resposta.choices[0].message.content)
+    print("Análise concluída com sucesso!")
+
 def carrega(nome_do_arquivo):
     try:
         with open(nome_do_arquivo, "r") as arquivo:
@@ -20,36 +55,6 @@ def salva(nome_do_arquivo, conteudo):
 dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-nome_do_produto = "Tapete de yoga"
-
-prompt_sistema = """
-Você é um analisador de sentimentos de avaliações de produtos.
-Escreva um parágrafo com até 50 palavras resumindo as avaliações e depois atribua qual o sentimento geral para o produto.
-Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
-
-#### Formato de saída
-
-Nome do produto: 
-Resumo das avaliações:
-Sentimento geral: [aqui deve ser POSITIVO, NEUTRO ou NEGATIVO]
-Pontos fortes: [3 bullet points]
-Pontos fracos: [3 bullet points]
-"""
-
-prompt_usuario = carrega(f"./dados/avaliacoes-{nome_do_produto}.txt")
-
-resposta = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
-    messages = [
-        {
-            "role": "system",
-            "content": prompt_sistema
-        },
-        {
-            "role": "user",
-            "content": prompt_usuario
-        }
-    ]
-)
-
-salva(f"./dados/analise-{nome_do_produto}", resposta.choices[0].message.content)
+lista_de_produtos = ["Tapete de yoga", "Tabuleiro de xadrez de madeira"]
+for nome_do_produto in lista_de_produtos:
+    analise_sentimento(nome_do_produto)
